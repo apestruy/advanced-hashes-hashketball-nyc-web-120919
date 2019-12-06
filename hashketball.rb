791657d1b7948b20c18f86410878f555dff39070
Alexandra Pestruyeva <apestruy@gmail.com>
2019-12-06 05:24:03 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -6,7 +6,7 @@ def game_hash
       :players => [{}]}, 
     :away => {
       :team_name => "Charlotte Hornets",
-      :colors => [],
+      :colors => ["Turquoise", "Purple"],
       :players => [{}]}
   
     
