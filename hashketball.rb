e79bb285c185d931fe461492c6046cdc2e2c5772
Alexandra Pestruyeva <apestruy@gmail.com>
2019-12-06 05:36:59 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -5,9 +5,10 @@ def game_hash
       :colors => ["Black", "White"], 
       :players => [
         {:player_name => "Alan Anderson", :number => 0, :shoe => 16, :points => 22, :rebounds => 12, :assists => 12, :steals => 3, :blocks => 1, :slam_dunks => 1}, 
+        {:player_name => "", :number => 5, :shoe => 5, :points => 5, :rebounds => 5, :assists => 5, :steals => 5, :blocks => 5, :slam_dunks => 5},
+        {:player_name => "", :number => 5, :shoe => 5, :points => 5, :rebounds => 5, :assists => 5, :steals => 5, :blocks => 5, :slam_dunks => 5},
+        {:player_name => "", :number => 5, :shoe => 5, :points => 5, :rebounds => 5, :assists => 5, :steals => 5, :blocks => 5, :slam_dunks => 5},
         {:player_name => "", :number => 5, :shoe => 5, :points => 5, :rebounds => 5, :assists => 5, :steals => 5, :blocks => 5, :slam_dunks => 5}
-        
-        
         ]}, 
     :away => {
       :team_name => "Charlotte Hornets",
