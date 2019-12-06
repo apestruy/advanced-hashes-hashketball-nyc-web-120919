7a09517a1a7c263eaeb768ac3559415f215e89ef
Alexandra Pestruyeva <apestruy@gmail.com>
2019-12-06 06:32:22 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -88,16 +88,16 @@ end
 
 def most_points_scored
   most_points = 0
-  points_owner = "name"
+  name = "name"
   game_hash.values.each do |team_info|
     team_info[:players].each do |player|
       if player[:points] > most_points
         most_points = player[:points] 
-        points_owner = player[:player_name] 
+        name = player[:player_name] 
       end
     end
   end
-  return points_owner
+  return name
 end 
 
 def winning_team
