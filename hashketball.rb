bb58f4ea93092d87c9b606fb0722add8aac39787
Alexandra Pestruyeva <apestruy@gmail.com>
2019-12-06 05:43:06 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -7,7 +7,7 @@ def game_hash
         {:player_name => "Alan Anderson", :number => 0, :shoe => 16, :points => 22, :rebounds => 12, :assists => 12, :steals => 3, :blocks => 1, :slam_dunks => 1}, 
         {:player_name => "Reggie Evans", :number => 30, :shoe => 14, :points => 12, :rebounds => 12, :assists => 12, :steals => 12, :blocks => 12, :slam_dunks => 7},
         {:player_name => "Brook Lopez", :number => 11, :shoe => 17, :points => 17, :rebounds => 19, :assists => 10, :steals => 3, :blocks => 1, :slam_dunks => 15},
-        {:player_name => "", :number => 5, :shoe => 5, :points => 5, :rebounds => 5, :assists => 5, :steals => 5, :blocks => 5, :slam_dunks => 5},
+        {:player_name => "Mason Plumlee", :number => 1, :shoe => 19, :points => 26, :rebounds => 11, :assists => 6, :steals => 3, :blocks => 1, :slam_dunks => 15},
         {:player_name => "", :number => 5, :shoe => 5, :points => 5, :rebounds => 5, :assists => 5, :steals => 5, :blocks => 5, :slam_dunks => 5}
         ]}, 
     :away => {
